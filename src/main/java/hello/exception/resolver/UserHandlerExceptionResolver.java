e30ff363bf5ca6c541afacc9456c5101ccc64d24
zerom-H <cupdisin@gmail.com>
2022-05-04 20:19:19 +0900

    HandlerExceptionResolver Add

--- src/main/java/hello/exception/resolver/UserHandlerExceptionResolver.java
+++ src/main/java/hello/exception/resolver/UserHandlerExceptionResolver.java
@@ -1,5 +1,6 @@
 package hello.exception.resolver;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import hello.exception.exception.UserException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.servlet.HandlerExceptionResolver;
@@ -14,6 +15,8 @@ import java.util.Map;
 @Slf4j
 public class UserHandlerExceptionResolver implements HandlerExceptionResolver {
 
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
     @Override
     public ModelAndView resolveException(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) {
         try {
@@ -26,9 +29,18 @@ public class UserHandlerExceptionResolver implements HandlerExceptionResolver {
                 if ("application/json".equals(acceptHeader)) {
                     Map<String, Object> errorResult = new HashMap<>();
                     errorResult.put("ex", ex.getClass());
-                    errorResult.put("mmeesage", ex.getMessage());
+                    errorResult.put("message", ex.getMessage());
 
+                    String result = objectMapper.writeValueAsString(errorResult);
 
+                    response.setContentType("application/json");
+                    response.setCharacterEncoding("utf-8");
+                    response.getWriter().write(result);
+
+                    return new ModelAndView();
+                } else {
+                    // 템플릿이 호출됨
+                    return new ModelAndView("error/500");
                 }
             }
         } catch (IOException e) {
